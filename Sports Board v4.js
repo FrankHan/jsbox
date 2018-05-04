cd8665b4631b5906e99a276d779069a6c2803ed6
Frank <hanqn09@163.com>
2018-05-04 10:03:52 +0800

    进一步精简代码

--- Sports Board v4.js
+++ Sports Board v4.js
@@ -8,7 +8,7 @@ let appVersion = 1.2
 // $app.tips("点击比赛即可查看详情"); 
 
 // 初始时获取NBA比赛
-getDatabyGametype("nba")
+getDatabyGametype("nba") //nba, chlg
 
 function getDatabyGametype(gametype) {
 
@@ -23,7 +23,7 @@ function getDatabyGametype(gametype) {
       "Proxy-Connection": "keep-alive",
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 isp/460.02 network/WIFI prokanqiu/7.1.18.55 iPhone8,4"
     },
-    handler: function(resp) {
+    handler: function (resp) {
 
       render(resp)
 
@@ -83,12 +83,6 @@ function getDatabyGametype(gametype) {
 
       var objOneDay_Rows = [];
 
-      var objOneDay = [];
-
-      var objOneDay = [{ //这必须是一个obj好像
-        title: "Section 0",
-        rows: objOneDay_Rows
-      }];
 
       var elementOneDay = { //这必须是一个obj element好像
         title: dateblockForSectionTitle,
@@ -98,10 +92,10 @@ function getDatabyGametype(gametype) {
       for (var i = 0; i < toDayList.length; i++) { // 当天的第0,1,2场比赛
 
         var obj = {};
-        // var arrToList = [];
+
         obj.teams = {};
         obj.content = {};
-        //      obj.gamename = {};
+
         obj.onewinscore = {};
         obj.twowinscore = {};
         obj.scheduleid = {};
@@ -121,158 +115,147 @@ function getDatabyGametype(gametype) {
           obj.gidUrl = toDayList[i].gid.toString();
           obj.isOver.text = toDayList[i].process;
           obj.content.text = toDayList[i].match_type;
-          
+
           //obj.content.text = toDayList[i].type_block;
           objOneDay_Rows.push(obj); //$$$
         }
 
-   
-
       }
-
-  
-
       rowToDayList.push(elementOneDay); //$$$2   https://segmentfault.com/q/1010000006791550  https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=push%E4%B9%8B%E5%90%8E%E5%8F%98%E6%88%90%E4%BA%86%E5%AD%97%E7%AC%A6%E4%B8%B2&oq=var%2520obj%2520%253D%2520%257B%257D%253B&rsv_pq=ec9d6f200001157f&rsv_t=bbd2CJwpyWLaqOSlTgiJcZvCannCBgnlH0q%2FKjDoOoUnbPx5oU9S5mzIXH4&rqlang=cn&rsv_enter=1&inputT=8775&rsv_sug3=66&rsv_sug1=27&rsv_sug7=100&rsv_sug2=0&rsv_sug4=8775
-
-     
 
     }
 
-   
-
     console.log(rowToDayList)
-
-   
 
     $ui.render({
 
       views: [{
-          type: "list",
-          props: {
-            id: "listid",
-            grouped: true,
-            rowHeight: 70, // 行高
-            header: {
-              type: "label",
-              props: {
-                height: 0,
-                text: "NBA赛程板", // headerDateTip,
-                textColor: $color("#AAAAAA"),
-                align: $align.center,
-                font: $font(14)
-              }
+        type: "list",
+        props: {
+          id: "listid",
+          grouped: true,
+          rowHeight: 70, // 行高
+          header: {
+            type: "label",
+            props: {
+              height: 0,
+              text: "NBA赛程板", // headerDateTip,
+              textColor: $color("#AAAAAA"),
+              align: $align.center,
+              font: $font(14)
+            }
+          },
+
+          template: [{
+            type: "label",
+            props: {
+              id: "isOver", // 进行中
+              font: $font(10),
+              textColor: $color("#888888")
             },
-
-            template: [{
-                type: "label",
-                props: {
-                  id: "isOver", // 进行中
-                  font: $font(10),
-                  textColor: $color("#888888")
-                },
-                layout: function(make, view) {
-                  make.centerX.equalTo(0)
-                  make.top.equalTo(2)
-                  // make.left.equalTo(160)
-                  // make.top.right.inset(8)
-                  make.height.equalTo(20)
-                }
-              }, {
-                type: "label",
-                props: {
-                  id: "teams", // 队伍
-                  font: $font(20)
-                },
-                layout: function(make, view) {
-                  make.centerX.equalTo(0)
-                  make.top.equalTo(18)
-                  // make.left.equalTo(160)
-                  // make.top.right.inset(8)
-                  make.height.equalTo(24)
-                }
-              },
-              {
-                type: "label",
-                props: {
-                  id: "content",
-                  textColor: $color("#888888"),
-                  font: $font(15)
-                }, // 比赛时间 id content
-                layout: function(make, view) {
-                  //make.left.right.equalTo(180);
-
-                  make.top.equalTo(48)
-
-                  make.centerX.equalTo(0) // 居中 
-                  make.bottom.equalTo(-2)
-                }
-              },
-              {
-                type: "label",
-                props: {
-                  id: "onewinscore", // 一队比分
-                  textColor: $color("#888888"),
-                  font: $font(20)
-                },
-                layout: function(make) {
-                  // make.left.inset(28)
-                  make.right.equalTo($("teams").centerX).offset(-110) //距离队伍的偏移量
-                  make.top.inset(10)
-                  make.height.equalTo(40)
-                }
-              },
-
-              {
-                type: "label",
-                props: {
-                  id: "twowinscore", // 二队比分
-                  textColor: $color("#888888"),
-                  font: $font(20)
-                },
-                layout: function(make) {
-                  //make.right.equalTo(40)
-                  // make.right.inset(28)
-                  make.left.equalTo($("teams").centerX).offset(110) //距离队伍的偏移量
-                  make.top.inset(10)
-                  make.height.equalTo(40)
-                }
-              }
-            ],
-            data: rowToDayList
-
+            layout: function (make, view) {
+              make.centerX.equalTo(0)
+              make.top.equalTo(2)
+              // make.left.equalTo(160)
+              // make.top.right.inset(8)
+              make.height.equalTo(20)
+            }
+          }, {
+            type: "label",
+            props: {
+              id: "teams", // 队伍
+              font: $font(20)
+            },
+            layout: function (make, view) {
+              make.centerX.equalTo(0)
+              make.top.equalTo(18)
+              // make.left.equalTo(160)
+              // make.top.right.inset(8)
+              make.height.equalTo(24)
+            }
           },
-          layout: function(make, view) {
-            make.left.right.equalTo(0);
-            make.top.equalTo(0);
-            make.height.equalTo(view.super);
-            make.bottom.equalTo(100);
-          },
-          events: {
-            didSelect: function(tableView, indexPath) {
-              var row = indexPath.row; // 目前这里不对
-              console.log(row)
-              var section = indexPath.section;
-              console.log(section)
-              //var scheduleid = rowToDayList(section,row).gidUrl;
+          {
+            type: "label",
+            props: {
+              id: "content",
+              textColor: $color("#888888"),
+              font: $font(15)
+            }, // 比赛时间 id content
+            layout: function (make, view) {
+              //make.left.right.equalTo(180);
 
-              var gidUrl = rowToDayList[section].rows[row].gidUrl;
-              console.log(gidUrl)
-              $ui.push({
-                props: {
-                  title: rowToDayList[section].rows[row].teams.text
-                },
-                views: [{
-                  type: "web",
-                  props: {
-                    url: "https://m.hupu.com/" + gametype + "/game/recap_" + gidUrl + ".html"
-                  },
-                  layout: $layout.fill,
-                }]
-              })
-            
+              make.top.equalTo(48)
+
+              make.centerX.equalTo(0) // 居中 
+              make.bottom.equalTo(-2)
+            }
+          },
+          {
+            type: "label",
+            props: {
+              id: "onewinscore", // 一队比分
+              textColor: $color("#888888"),
+              font: $font(20)
+            },
+            layout: function (make) {
+              // make.left.inset(28)
+              make.right.equalTo($("teams").centerX).offset(-110) //距离队伍的偏移量
+              make.top.inset(10)
+              make.height.equalTo(40)
+            }
+          },
+
+          {
+            type: "label",
+            props: {
+              id: "twowinscore", // 二队比分
+              textColor: $color("#888888"),
+              font: $font(20)
+            },
+            layout: function (make) {
+              //make.right.equalTo(40)
+              // make.right.inset(28)
+              make.left.equalTo($("teams").centerX).offset(110) //距离队伍的偏移量
+              make.top.inset(10)
+              make.height.equalTo(40)
             }
           }
+          ],
+          data: rowToDayList
+
+        },
+        layout: function (make, view) {
+          make.left.right.equalTo(0);
+          make.top.equalTo(0);
+          make.height.equalTo(view.super);
+          make.bottom.equalTo(100);
+        },
+        events: {
+          didSelect: function (tableView, indexPath) {
+            var row = indexPath.row; // 目前这里不对
+            console.log(row)
+            var section = indexPath.section;
+            console.log(section)
+            //var scheduleid = rowToDayList(section,row).gidUrl;
+
+            var gidUrl = rowToDayList[section].rows[row].gidUrl;
+            console.log(gidUrl)
+            $ui.push({
+              props: {
+                title: rowToDayList[section].rows[row].teams.text
+              },
+              views: [{
+                type: "web",
+                props: {
+                  url: "https://m.hupu.com/" + gametype + "/game/recap_" + gidUrl + ".html"
+                },
+                layout: $layout.fill,
+              }]
+            })
+
+          }
         }
+      }
 
       ]
     })
